d88bfa861a02e6b4b883d5aba8bbf578d4afbc21
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2023-02-02 23:17:55 -0300

    init detect texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,92 @@
-import React, { useEffect, useState, useRef } from "react";
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
+import * as tesseract from "tesseract.js";
 import Webcam from "react-webcam";
 
-function App() {
-	const [model, setModel] = useState<cocoSsd.ObjectDetection>();
-	const webcamRef = useRef<any>(null);
-	const [videoWidth, setVideoWidth] = useState(960);
-	const [videoHeight, setVideoHeight] = useState(640);
-	const videoConstraints = {
-		height: 1080,
-		width: 1920,
-		facingMode: "environment",
+const App = () => {
+	const [result, setResult] = useState<any>();
+	const [textResult, setTextResult] = useState<string[]>([]);
+
+	const webcamRef: any = useRef(null);
+
+	const capture = useCallback(
+		() => {
+			const imageSrc = webcamRef.current?.getScreenshot();
+			return imageSrc;
+		},
+		[webcamRef]
+	);
+	const extractTextFromCNH = async (image: tesseract.ImageLike) => {
+		let text = await tesseract.recognize(image, "por");
+
+		return text;
 	};
 
-	async function loadModel() {
-		try {
-			const model = await cocoSsd.load();
-			setModel(model);
-		} catch (err) {
-			console.error(err);
-			console.log("failed load model");
-		}
-	}
-
 	useEffect(() => {
-		tf.ready().then(() => {
-			loadModel();
-		});
-	}, []);
- 
-	async function predictionFunction() {
-		//Clear the canvas for each prediction
-		var cnvs = document.getElementById("myCanvas") as HTMLCanvasElement;
-		var ctx = cnvs?.getContext("2d");
-		ctx?.clearRect(
-			0,
-			0,
-			webcamRef.current.video.videoWidth,
-			webcamRef.current.video.videoHeight
-		);
-		//Start prediction
-		const predictions = await model?.detect(document.getElementById("img") as HTMLImageElement);
-		console.log("🚀 ~ file: App.tsx:54 ~ predictionFunction ~ predictions", predictions)
-		if (predictions && predictions.length > 0) {
-			console.log(predictions);
-			for (let n = 0; n < predictions.length; n++) {
-				console.log(n, ctx, predictions[n].score);
-				if (ctx && predictions[n].score > 0.7) {
-					//Threshold is 0.7 or 70%
-					//Extracting the coordinate and the bounding box information
-					let bboxLeft = predictions[n].bbox[0];
-					let bboxTop = predictions[n].bbox[1];
-					let bboxWidth = predictions[n].bbox[2] - (bboxLeft * 2);
-					let bboxHeight = predictions[n].bbox[3] - bboxTop;
-					console.log("bboxLeft: " + bboxLeft);
-					console.log("bboxTop: " + bboxTop);
-					console.log("bboxWidth: " + bboxWidth);
-					console.log("bboxHeight: " + bboxHeight);
-					//Drawing begin
-					ctx.beginPath();
-					ctx.font = "28px Arial";
-					ctx.fillStyle = "red";
-					ctx.fillText(
-						predictions[n].class +
-							": " +
-							Math.round(parseFloat(String(predictions[n].score)) * 100) +
-							"%",
-						bboxLeft,
-						bboxTop
-					);
-					ctx.rect(bboxLeft, bboxTop, bboxWidth, bboxHeight);
-					ctx.strokeStyle = "#FF0000";
-					ctx.lineWidth = 3;
-					ctx.stroke();
-					console.log("detected");
+		async function run() {
+			// Obtém a referência da webcam
+			const video: any = webcamRef.current;
+
+			// tira um print da webcam
+			let image = capture();
+
+			// extrai o texto da imagem
+			extractTextFromCNH(image).then((text) => {
+				if (text.data.text) {
+					// se a previsão for maior que 0.5, então é uma pessoa
+					if (text.data.text.length > 0) {
+						let texts = [];
+						for (let line of text.data.lines) {
+							if (line.confidence > 70) {
+								texts.push(line.text);
+							}
+						}
+						setTextResult(texts);
+					}
 				}
-			}
-		}
-		//Rerun prediction by timeout
-		setTimeout(() => predictionFunction(), 500);
-	}
+			});
 
-	useEffect(() => {
-		setVideoWidth(webcamRef.current?.video.videoWidth);
-		setVideoHeight(webcamRef.current?.video.videoHeight);
-		console.log("🚀 ~ webcamRef.current?.video.videoWidth", webcamRef.current?.video.videoWidth, webcamRef.current?.video.videoHeight)
-	}, [webcamRef.current?.video.videoWidth]);
+			// Aguarda o carregamento do video
+			await new Promise((resolve) => (video.onloadedmetadata = resolve));
+
+			// Define o modelo
+			const model = tf.sequential();
+			model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+			// Compila o modelo
+			model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+
+			// Define os dados de treinamento
+			const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+			const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+			// Treina o modelo
+			await model.fit(xs, ys, { epochs: 10 });
+
+			// Faz previsões
+			const result = model.predict(tf.tensor2d([5], [1, 1]));
+
+			// Exibe o resultado
+			// setResult(result.dataSync()[0]);
+		}
+
+		// loop
+		setInterval(run, 1300);
+	}, []);
 
 	return (
-		<div className="App">
-
-			<button onClick={() => predictionFunction()}>Start</button>
-
-			<div style={{ position: "absolute", top: "50px" }}>
-				<Webcam
-					audio={false}
-					id="img"
-					ref={webcamRef}
-					screenshotQuality={1}
-					screenshotFormat="image/jpeg"
-					videoConstraints={videoConstraints}
-				/>
-			</div>
-			<div style={{ position: "absolute", top: "50px", zIndex: "9999" }}>
-				<canvas
-					id="myCanvas"
-					width={videoWidth}
-					height={videoHeight}
-					style={{ backgroundColor: "transparent" }}
-				/>
-			</div>
+		<div>
+			<Webcam
+				ref={webcamRef}
+				audio={false}
+				screenshotFormat="image/jpeg"
+			/>
+			<h1>Resultado:</h1>
+			{result ? <p>{String(result)}</p> : <p>Carregando...</p>}
+			<h1>Texto:</h1>
+			{textResult ? <p>{String(textResult)}</p> : <p>Carregando...</p>}
 		</div>
 	);
-}
+};
 
 export default App;
